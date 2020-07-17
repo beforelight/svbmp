644ea5ea17881bb84789b8dc85c14c033fec0385
xiao <1761690868@qq.com>
2020-07-17 19:23:59 +0800

    init

---
+++ bmpCreater.c
@@ -0,0 +1,71 @@
+﻿// bmpCreater.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include"stdio.h"
+#include"svbmp.h"
+#include"stdlib.h"
+#include"string.h"
+
+#define RGB565(r,g,b) (uint16_t)(((r<<8)&0xF800U)|((g<<3)&0x7E0U)|((b>>3)&0x1FU))
+int main()
+{
+	//https://www.cnblogs.com/wainiwann/p/7086844.html
+	sizeof(BITMAPFILEHEADER);
+	sizeof(BITMAPINFOHEADER);
+	sizeof(BITMAPGRAYHEADER);
+	sizeof(BITMAPRGBHEADER);
+
+
+
+	img_t a, b;
+	FILE* abmp, *bbmp;
+
+
+	//灰度图像测试
+	a.format = PixelFormatGray;
+	a.height = 120;
+	a.width = 184;
+	a.pImg = malloc(a.height * a.width);
+	memset(a.pImg, 0, a.height * a.width);
+	for (size_t i = 0; i < a.width; i++)
+	{
+		((uint8_t*)a.pImg)[40 * a.width + i] = 255;
+		((uint8_t*)a.pImg)[39 * a.width + i] = 128;
+	}
+
+	fopen_s(&abmp, "a.bmp", "wb");
+	BMP_Save(abmp, &a);
+	fclose(abmp);
+
+
+	//彩色图像测试
+	b.format = PixelFormatRGB565;
+	b.height = 240;
+	b.width = 320;
+	b.pImg = malloc(b.height * b.width * 2);
+	memset(b.pImg, 0, b.height * b.width * 2);
+	for (size_t i = 0; i < b.width; i++)
+	{
+		((uint16_t*)b.pImg)[38 * b.width + i] = RGB565(255,0,0);
+		((uint16_t*)b.pImg)[39 * b.width + i] = RGB565(0,255,0);
+		((uint16_t*)b.pImg)[40 * b.width + i] = RGB565(0,0,255);
+	}
+
+	fopen_s(&bbmp, "b.bmp", "wb");
+	BMP_Save(bbmp, &b);
+	fclose(bbmp);
+
+
+	free(a.pImg);
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ svbmp.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include <stdint.h>
+#ifdef WIN32
+typedef enum _camera_pixel_format
+{
+	PixelFormatGray = 1,//灰度
+	PixelFormatRGB565 = 2,
+}camera_pixel_format_t;
+typedef struct __img {
+	camera_pixel_format_t format;
+	uint16_t height;
+	uint16_t width;
+	void* pImg;
+}img_t;
+#include <stdio.h>
+#define FIL FILE
+#else
+#include"sc_camera_common.h"
+#endif // WIN32
+
+#pragma  pack(1)
+typedef struct tagBITMAPFILEHEADER {
+	/* bmfh 14bytes */
+	uint16_t bfType;
+	uint32_t bfSize;
+	uint16_t bfReserved1;
+	uint16_t bfReserved2;
+	uint32_t bfOffBits;
+} BITMAPFILEHEADER;
+
+#pragma  pack(1)
+typedef struct tagBITMAPINFOHEADER {
+	/* bmih 54bytes */
+	BITMAPFILEHEADER bmfh;
+	uint32_t biSize;
+	int32_t biWidth;
+	int32_t biHeight;
+	uint16_t biPlanes;
+	uint16_t biBitCount;
+	uint32_t biCompression;
+	uint32_t biSizeImage;
+	int32_t biXPelsPerMeter;
+	int32_t biYPelsPerMeter;
+	uint32_t biClrUsed;
+	uint32_t biClrImportant;
+} BITMAPINFOHEADER;
+
+#pragma  pack(1)
+typedef struct tagBITMAPGRAYHEADER {
+	/* bmgh 1080bytes */
+	//BITMAPINFOHEADER bmif;
+	uint32_t bmColors[256];
+} BITMAPGRAYHEADER;
+
+#pragma  pack(1)
+typedef struct tagBITMAPRGBHEADER {
+	/* bmrh 72bytes */
+	//BITMAPINFOHEADER bmif;
+	uint32_t bmColors[4];
+} BITMAPRGBHEADER;
+
+
+int BMP_FileWrite(FIL* fp, const void* buff, uint32_t btw);
+int BMP_Save(FIL* fp, img_t* img);
